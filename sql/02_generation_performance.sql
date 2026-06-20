b0a59156f2a0b328e93d1a333a887f51622bc0ad
SharonkarmalLouis <karmallouis@gmail.com>
2026-06-20 08:58:43 +0100

    Add site generation performance analysis

--- sql/02_generation_performance.sql
+++ sql/02_generation_performance.sql
@@ -1,1 +1,9 @@
--- To be developed
+-- Total Energy Generation by Site
+
+SELECT
+    site_id,
+    ROUND(SUM(energy_mwh), 2) AS total_energy_generated_mwh,
+    ROUND(AVG(availability_percent), 2) AS avg_availability
+FROM `renewable-energy-analytics.renewable_energy.energy_generation`
+GROUP BY site_id
+ORDER BY total_energy_generated_mwh DESC;
