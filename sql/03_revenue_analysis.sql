c4baaa59dc65b4459b88cf443e3c63b3c2222ae0
SharonkarmalLouis <karmallouis@gmail.com>
2026-06-20 10:31:08 +0100

    Update 03_revenue_analysis.sql

--- sql/03_revenue_analysis.sql
+++ sql/03_revenue_analysis.sql
@@ -30,3 +30,30 @@ JOIN `renewable-energy-analytics.renewable_energy.market_prices` mp
     ON eg.generation_date = mp.price_date
 GROUP BY eg.site_id
 ORDER BY revenue_rank;
+
+-- Query 3: Revenue Efficiency by Site
+
+SELECT
+    eg.site_id,
+    ROUND(
+        SUM(eg.energy_mwh * mp.market_price_per_mwh),
+        2
+    ) AS estimated_revenue,
+    ROUND(
+        SUM(eg.energy_mwh),
+        2
+    ) AS total_generation_mwh,
+    ROUND(
+        SUM(eg.energy_mwh * mp.market_price_per_mwh)
+        / SUM(eg.energy_mwh),
+        2
+    ) AS revenue_per_mwh
+FROM `renewable-energy-analytics.renewable_energy.energy_generation` eg
+JOIN `renewable-energy-analytics.renewable_energy.market_prices` mp
+    ON eg.generation_date = mp.price_date
+GROUP BY eg.site_id
+ORDER BY revenue_per_mwh DESC;
+
+-- Purpose:
+-- Measure revenue earned per MWh generated.
+-- Useful for comparing revenue efficiency across sites.
