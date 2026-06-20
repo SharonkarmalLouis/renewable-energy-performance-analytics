e7675fec38ff0664589e27a4f860e69d826836f6
SharonkarmalLouis <karmallouis@gmail.com>
2026-06-20 08:20:49 +0100

    Add initial site and asset analysis query
    Added a SQL query to summarize site and asset data.

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,1 +1,13 @@
-
+-- Site and Asset Summary
+SELECT
+    s.site_name,
+    s.technology,
+    COUNT(a.asset_id) AS total_assets,
+    ROUND(SUM(a.capacity_mw),2) AS total_capacity_mw
+FROM `renewable-energy-analytics.renewable_energy.Sites` s
+LEFT JOIN `renewable-energy-analytics.renewable_energy.assets` a
+    ON s.site_id = a.site_id
+GROUP BY
+    s.site_name,
+    s.technology
+ORDER BY total_capacity_mw DESC;
