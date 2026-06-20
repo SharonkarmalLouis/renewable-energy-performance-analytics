434e5c371e3eaeecfbfba24a5c3d3e83fb713d39
SharonkarmalLouis <karmallouis@gmail.com>
2026-06-20 10:16:53 +0100

    Add query for bottom 10 performing assets

--- sql/02_generation_performance.sql
+++ sql/02_generation_performance.sql
@@ -54,4 +54,21 @@ GROUP BY asset_id
 ORDER BY performance_rank
 LIMIT 10;
 
+-- Query 5: Bottom 10 Performing Assets
+SELECT
+    asset_id,
+    ROUND(SUM(energy_mwh),2) AS actual_generation,
+    ROUND(SUM(expected_energy_mwh),2) AS expected_generation,
+    ROUND(
+        (SUM(energy_mwh) / SUM(expected_energy_mwh)) * 100,
+        2
+    ) AS performance_percent,
+    RANK() OVER (
+        ORDER BY
+            (SUM(energy_mwh) / SUM(expected_energy_mwh)) ASC
+    ) AS performance_rank
+FROM `renewable-energy-analytics.renewable_energy.energy_generation`
+GROUP BY asset_id
+ORDER BY performance_rank
+LIMIT 10;
 
