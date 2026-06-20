83b84b7a921650204cfb130cdffe79166344d3b6
SharonkarmalLouis <karmallouis@gmail.com>
2026-06-20 09:01:30 +0100

    Add actual vs expected generation performance query

--- sql/02_generation_performance.sql
+++ sql/02_generation_performance.sql
@@ -6,3 +6,17 @@ SELECT
 FROM `renewable-energy-analytics.renewable_energy.energy_generation`
 GROUP BY site_id
 ORDER BY total_energy_generated_mwh DESC;
+
+
+-- Actual vs Expected Generation Performance
+SELECT
+    site_id,
+    ROUND(SUM(energy_mwh),2) AS actual_generation,
+    ROUND(SUM(expected_energy_mwh),2) AS expected_generation,
+    ROUND(
+        (SUM(energy_mwh) / SUM(expected_energy_mwh)) * 100,
+        2
+    ) AS performance_percent
+FROM `renewable-energy-analytics.renewable_energy.energy_generation`
+GROUP BY site_id
+ORDER BY performance_percent DESC;
