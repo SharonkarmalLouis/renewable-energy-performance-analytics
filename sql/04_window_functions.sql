e174bcd59dd9179d34368b4e1234458fc622cbdc
SharonkarmalLouis <karmallouis@gmail.com>
2026-06-20 10:01:53 +0100

    Add site performance ranking using window functions
    Add SQL query for site performance ranking using window functions.

--- sql/04_window_functions.sql
+++ sql/04_window_functions.sql
@@ -1,1 +1,16 @@
--- To be developed
+-- Site Performance Ranking
+SELECT
+    site_id,
+    ROUND(SUM(energy_mwh),2) AS actual_generation,
+    ROUND(SUM(expected_energy_mwh),2) AS expected_generation,
+    ROUND(
+        (SUM(energy_mwh) / SUM(expected_energy_mwh)) * 100,
+        2
+    ) AS performance_percent,
+    RANK() OVER (
+        ORDER BY
+            (SUM(energy_mwh) / SUM(expected_energy_mwh)) DESC
+    ) AS performance_rank
+FROM `renewable-energy-analytics.renewable_energy.energy_generation`
+GROUP BY site_id
+ORDER BY performance_rank;
